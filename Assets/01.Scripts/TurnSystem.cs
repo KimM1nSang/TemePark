d3f35bbae9420ad032ba5faedf909b783a80a03a
KimM1nSang <minsang1008@naver.com>
2021-12-23 01:20:47 +0900

    이동 완료

--- Assets/01.Scripts/TurnSystem.cs
+++ Assets/01.Scripts/TurnSystem.cs
@@ -2,18 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using DG.Tweening;
 
 public enum TurnState {
     START,
     PLAYERTURN,
     ENEMYTURN,
-    BETWEEN,
     DIRCHOISE,
     WON,
     LOST }
 public class TurnSystem : MonoBehaviour
 {
     public static TurnSystem Instance;
+
     [SerializeField]
     private GameObject playerPrefab;
     [SerializeField]
@@ -22,8 +23,8 @@ public class TurnSystem : MonoBehaviour
     [SerializeField] private TileNode startTile;
     public TurnState state;
 
-    private Unit playerUnit;
-    private Unit enemyUnit;
+    public Unit playerUnit { get; set; }
+    public Unit enemyUnit { get; set; }
 
 
     void Start()
@@ -45,10 +46,12 @@ public class TurnSystem : MonoBehaviour
         GameObject playerGO = Instantiate(playerPrefab, startTile.unitsPosTrms[0]);
         playerUnit = playerGO.GetComponent<Unit>();
         playerUnit.currentTile = startTile;
+        playerUnit.state = UnitState.PLAYER;
 
         GameObject enemyGO = Instantiate(enemyPrefab, startTile.unitsPosTrms[1]);
         enemyUnit= enemyGO.GetComponent<Unit>();
         enemyUnit.currentTile = startTile;
+        enemyUnit.state = UnitState.ENEMY;
 
         yield return new WaitForSeconds(2f);
 
@@ -76,66 +79,112 @@ public class TurnSystem : MonoBehaviour
 
     public void DiceCallback(Unit unit)
     {
-        Debug.Log(unit.dice.currentNum+1);
-        Moving(unit, playerUnit.currentTile, playerUnit.dice.currentNum + 2);
+        Debug.Log("ÁÖ»çÀ§ ´« : " + (unit.dice.currentNum+1));
+        Moving(unit, unit.currentTile, unit.dice.currentNum + 1);
     }
 
     public void Moving(Unit unit, TileNode node,int index, int dir = 0)
     {
-        if ( node.nextTileNodes.Length > 0)
+        int leng = node.nextTileNodes.Length;
+        print(leng);
+        print(node.name);
+        if (leng > 0  && leng <= 1 && index > 0)
         {
-            if(index > 0)
-            {
-                index--;
-                unit.index = index;
-                if (state != TurnState.DIRCHOISE)
-                {
-                    state = TurnState.DIRCHOISE;
-                    unit.OnMove += Moving;
-                    unit.MoveTile();
-                }
-                else
-                {
-                    dir = node.nextTileNodes.Length > 1 ? dir: 0;
+            index--;
 
-                    print(dir);
-                    Debug.Log(node.gameObject.name);
-                    Debug.Log(node.nextTileNodes[dir].gameObject.name);
-                    node = node.nextTileNodes[dir];
-                    unit.nodeWay.Enqueue(node);
-                    Moving(unit, node, index, dir);
-                }
+            //dir = node.nextTileNodes.Length > 1 ? dir : 0;
+            dir = 0;
+            node = node.nextTileNodes[dir];
+            unit.nodeWay.Enqueue(node);
+            print(unit.nodeWay.Count);
+            Moving(unit, node, index, dir);
+        }
+        else
+        {
+            print("¹«ºù");
+            print(unit.nodeWay.Count);
+           MoveTile(unit, index);
+        }
+    }
+    public void MoveTile(Unit unit,int index)
+    {
+        print(unit.nodeWay.Count);
+        if (unit.nodeWay.Count <= 0)
+        {
+            print("À¯´Ö ³ëµå ÀúÀå¼Ò ºñ¾úÀ½");
+            if(index > 0 && unit.currentTile.nextTileNodes.Length > 1)
+            {
+                StartCoroutine(ChoiseDir(unit, index));
             }
             else
             {
-                unit.MoveTile();
+                EndOfTurn(unit);
             }
         }
         else
         {
-            unit.MoveTile();
+            unit.transform.DOMove(unit.nodeWay.Peek().TileCenterTrm.position, unit.moveDuration).OnComplete(() => {
+                unit.currentTile = unit.nodeWay.Dequeue(); MoveTile(unit,index);
+            });
         }
+
     }
 
-    public void EndOfTurn()
+    private IEnumerator ChoiseDir(Unit unit,int index)
     {
-        switch (state)
+        bool isCor = true;
+        print("A = 0, S = 1");
+        //print(index);
+        while (isCor)
         {
-            case TurnState.START:
-                break;
-            case TurnState.PLAYERTURN:
+            yield return new WaitForEndOfFrame();
+            if(state == TurnState.PLAYERTURN)
+            {
+                if (Input.GetKey(KeyCode.A))
+                {
+                    print("A = 0");
+                    isCor = false;
+                    unit.dir = 0;
+                }
+                else if (Input.GetKey(KeyCode.S))
+                {
+                    print("S = 1");
+                    isCor = false;
+                    unit.dir = 1;
+                }
+            }
+            else if (state == TurnState.ENEMYTURN)
+            {
+                yield return new WaitForSeconds(1f);
+                isCor = false;
+                unit.dir = UnityEngine.Random.Range(0, 1);
+            }
+
+        }
+        index--;
+        unit.nodeWay.Enqueue(unit.currentTile.nextTileNodes[unit.dir]);
+        Moving(unit, unit.currentTile.nextTileNodes[unit.dir], index, unit.dir);
+    }
+
+    public void EndOfTurn(Unit unit)
+    {
+        switch (unit.state)
+        {
+            case UnitState.PLAYER:
                 state = TurnState.ENEMYTURN;
+                StartCoroutine(EnemyTurn());
                 break;
-            case TurnState.ENEMYTURN:
-                break;
-            case TurnState.BETWEEN:
-                break;
-            case TurnState.WON:
-                break;
-            case TurnState.LOST:
+            case UnitState.ENEMY:
+                state = TurnState.PLAYERTURN;
                 break;
             default:
                 break;
         }
+    }
+    private IEnumerator EnemyTurn()
+    {
+        enemyUnit.dice.Roll();
+        yield return new WaitForSeconds(1.5f);
+        enemyUnit.dice.Select();
     }
 }
--- Assets/01.Scripts/Unit.cs
+++ Assets/01.Scripts/Unit.cs
@@ -4,23 +4,25 @@ using UnityEngine;
 using DG.Tweening;
 using System;
 
+public enum UnitState
+{
+    PLAYER,
+    ENEMY
+}
 public class Unit : MonoBehaviour
 {
+    public UnitState state = UnitState.PLAYER;
+
     public GameObject diceObj;
     public Dice dice { get; set; }
     [SerializeField] private Transform DiceTrm;
 
-    public Queue<TileNode> nodeWay { get; set; }
+    public Queue<TileNode> nodeWay;
     public TileNode currentTile;
 
-    private float moveDuration = 2;
+    public float moveDuration = 2;
 
-    public delegate void CallBack(Unit unit, TileNode node, int index, int dir = 0);
-
-    public int index;
     public int dir = 0;
-
-    public CallBack OnMove { get; set; }
 
     private void Start()
     {
@@ -31,58 +33,6 @@ public class Unit : MonoBehaviour
 
         nodeWay = new Queue<TileNode>();
 
-        OnMove += ClearOnMove;
     }
-    public void MoveTile()
-    {
-        if (nodeWay.Count <= 0)
-        {
-            Debug.Log("µµÂø");
-            if(TurnSystem.Instance.state == TurnState.DIRCHOISE)
-            {
-                if(index > 0 &&currentTile.nextTileNodes.Length > 0)
-                {
-                    StartCoroutine(ChoiseDir());
-                }
-            }
-        }
-        else
-        {
-            print("AAAAAA");
-            transform.DOMove(nodeWay.Peek().TileCenterTrm.position, moveDuration).OnComplete(() => {
-                currentTile = nodeWay.Dequeue(); MoveTile(); 
-            });
-        }
-
-    }
-
-    private IEnumerator ChoiseDir()
-    {
-        bool isCor = true;
-        print("A = 0, S = 1");
-        print(index);
-        while (isCor)
-        {
-            yield return new WaitForEndOfFrame();
-            if(Input.GetKey(KeyCode.A))
-            {
-                print("A = 0");
-                isCor = false;
-                dir = 0;
-            }
-            else if(Input.GetKey(KeyCode.S))
-            {
-                print("S = 1");
-                isCor = false;
-                dir = 1;
-            }
-        }
-        OnMove?.Invoke(this, currentTile, index, dir);
-    }
-    private void ClearOnMove(Unit unit, TileNode node, int index, int dir = 0)
-    {
-        OnMove = null;
-        OnMove += ClearOnMove;
-    }
-
+  
 }
